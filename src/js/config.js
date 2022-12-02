48da7b4b5b24cd0ec7b9c929f484534e75b3898c
Hammad Ahmed <110901260+HammadEngr@users.noreply.github.com>
2022-12-02 17:11:38 +0500

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '19f00629-1721-4cfb-855c-6686ffaf7769';
+export const MODAL_CLOSE_SEC = 2.5;
